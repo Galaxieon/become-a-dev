4ce784e200c59face090a2d46d0ee65df619b6ea
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-08-25 16:52:00 +0530

    #become a dev logo added to icons folder #become a dev home page created #added responsive

---
+++ becomeadev/script.js
@@ -0,0 +1,11 @@
+function showMore() {
+    document.getElementById("more").style.display = "block";
+    document.getElementById("less-btn").style.display = "block"
+    document.getElementById("more-btn").style.display = "none"
+    document.getElementById("alert").style.display = "block"
+}
+function showLess() {
+    document.getElementById("more").style.display = "none";
+    document.getElementById("less-btn").style.display = "none"
+    document.getElementById("more-btn").style.display = "block"
+}
