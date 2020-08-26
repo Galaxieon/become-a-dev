7cd94bfd02fa3443a7058d648d6e3980c5f29c04
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-08-26 19:07:13 +0530

    # half of chapter 1 completed # added sticky nav bar to all become a dev pages # added media query # added some images

--- becomeadev/script.js
+++ becomeadev/script.js
@@ -1,11 +1,30 @@
 function showMore() {
     document.getElementById("more").style.display = "block";
-    document.getElementById("less-btn").style.display = "block"
-    document.getElementById("more-btn").style.display = "none"
-    document.getElementById("alert").style.display = "block"
+    document.getElementById("less-btn").style.display = "block";
+    document.getElementById("more-btn").style.display = "none";
+    document.getElementById("alert").style.display = "block";
 }
 function showLess() {
     document.getElementById("more").style.display = "none";
-    document.getElementById("less-btn").style.display = "none"
-    document.getElementById("more-btn").style.display = "block"
+    document.getElementById("less-btn").style.display = "none";
+    document.getElementById("more-btn").style.display = "block";
+}
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+    myFunction();
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
 }
